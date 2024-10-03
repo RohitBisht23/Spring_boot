149d1e8194501323a4d6b346f8bf17b519569d55
rohitbisht0911@gmail.com <rohitbisht0911@gmail.com>
2024-10-03 23:57:58 +0530

    first commit

---
+++ introductionToSpringBoot/src/test/java/com/learningSpringBoot/rohit/firstTime/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningSpringBoot.rohit.firstTime.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
