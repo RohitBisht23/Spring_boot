5609cd1388103d3890ce7fa30215dace410bcc06
rohitbisht0911@gmail.com <rohitbisht0911@gmail.com>
2024-10-05 22:23:58 +0530

    added all files

---
+++ introductionToSpringBoot/src/main/java/com/learningSpringBoot/rohit/firstTime/introductionToSpringBoot/Apple.java
@@ -0,0 +1,10 @@
+package com.learningSpringBoot.rohit.firstTime.introductionToSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+    void eat() {
+        System.out.println("You can eat the apple");
+    }
+}
--- introductionToSpringBoot/src/main/java/com/learningSpringBoot/rohit/firstTime/introductionToSpringBoot/IntroductionToSpringBootApplication.java
+++ introductionToSpringBoot/src/main/java/com/learningSpringBoot/rohit/firstTime/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -1,14 +1,22 @@
 package com.learningSpringBoot.rohit.firstTime.introductionToSpringBoot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionToSpringBootApplication {
+public class IntroductionToSpringBootApplication implements CommandLineRunner {
 
+	@Autowired
+	Apple appleObj;
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		appleObj.eat();
+	}
 }
