0e24983c0b59b138998f16217e0bbcc47f568bdf
Rohit Bisht <127304460+RohitBisht23@users.noreply.github.com>
2024-10-11 22:29:58 +0530

    Week 2 -> Presentation Layer, DTO, Controller

---
+++ SpringBootWebWeekTwo/SpringBootWebWeekTwo/src/main/java/com/week_two/SpringBootWebWeekTwo/SpringBootWebWeekTwo/SpringBootWebWeekTwoApplication.java
@@ -0,0 +1,13 @@
+package com.week_two.SpringBootWebWeekTwo.SpringBootWebWeekTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebWeekTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebWeekTwoApplication.class, args);
+	}
+
+}
---
+++ SpringBootWebWeekTwo/SpringBootWebWeekTwo/src/test/java/com/week_two/SpringBootWebWeekTwo/SpringBootWebWeekTwo/SpringBootWebWeekTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week_two.SpringBootWebWeekTwo.SpringBootWebWeekTwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebWeekTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
